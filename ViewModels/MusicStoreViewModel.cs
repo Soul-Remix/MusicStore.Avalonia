af4c5c42118a26d07b9ce45a6c2f2ad214cbf846
Soul-Remix <soulremix-dev@pm.me>
2024-02-16 19:22:18 +0300

    Update music store view

--- ViewModels/MusicStoreViewModel.cs
+++ ViewModels/MusicStoreViewModel.cs
@@ -1,8 +1,39 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using ReactiveUI;
 
 namespace MusicStore.Avalonia.ViewModels;
 
 public class MusicStoreViewModel : ViewModelBase
 {
+    private string? _searchText;
+    private bool _isBusy;
 
+    public string? SearchText
+    {
+        get => _searchText;
+        set => this.RaiseAndSetIfChanged(ref _searchText, value);
+    }
+    public bool IsBusy
+    {
+        get => _isBusy;
+        set => this.RaiseAndSetIfChanged(ref _isBusy, value);
+    }
+
+    private AlbumViewModel? _selectedAlbum;
+
+    public ObservableCollection<AlbumViewModel> SearchResults { get; } = [];
+
+    public AlbumViewModel? SelectedAlbum
+    {
+        get => _selectedAlbum;
+        set => this.RaiseAndSetIfChanged(ref _selectedAlbum, value);
+    }
+
+    public MusicStoreViewModel()
+    {
+        SearchResults.Add(new AlbumViewModel());
+        SearchResults.Add(new AlbumViewModel());
+        SearchResults.Add(new AlbumViewModel());
+    }
 }
